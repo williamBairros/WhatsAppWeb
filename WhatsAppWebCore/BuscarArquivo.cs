9b9816a100d445d87fd6991831504c8d7b855467
William Bairros Rodrigues <williambairros@live.com>
2021-10-15 20:45:49 -0300

    Adicionado Projetos Versão .NetCore Adicinando os campos  SegundosDeProcura,TipoDeProcura e suas funcionalidades

---
+++ WhatsAppWebCore/BuscarArquivo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace WhatsAppWeb
+{
+    public class BuscarArquivo
+    {
+        public string DiretorioArquivos { get; set; }
+        public Dictionary<int,string> Campos { get; set; }
+        public string Delimitador { get; set; }
+    }
+}
